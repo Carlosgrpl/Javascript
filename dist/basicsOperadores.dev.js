2b34ebca72b47b061d7fe6c2b54a9097396e1fec
Carlos Luis Gonzalez-Rubio <carlos.gonzalezrubio@bakertilly.es>
2024-04-10 19:00:26 +0200

    holi

--- dist/basicsOperadores.dev.js
+++ dist/basicsOperadores.dev.js
@@ -1,33 +1,44 @@
-console.log("Hello World");
-//alert("stawp");
+"use strict";
+
+console.log("Hello World"); //alert("stawp");
+
 /* Alert: hace que se muestre una alerta en la web, no se suele usar mucho... */
+
 /* shitf alf f: Prettier -> Formatear el documento, antes de terminar siempre formatear el código */
+
 /* let username = "Carlos"; */
+
 /* confirm("Are you sure?"); */
-let userName = "Carlos"; /* aqui creamos la variable, info de tipo STRING */
-let userAge = 25; /* aqui creamos la variable, info de tipo NUMBER */
-let isOnline = true; /* aqui creamos la variable, info de tipo BOOLEAN */
+
+var userName = "Carlos";
+/* aqui creamos la variable, info de tipo STRING */
+
+var userAge = 25;
+/* aqui creamos la variable, info de tipo NUMBER */
+
+var isOnline = true;
+/* aqui creamos la variable, info de tipo BOOLEAN */
+
 /* las variables en JS se crean del mismo modo: 1. let, 2. nombre, 3. = "tipo de dato"; */
 
 console.log("User Name: ", userName);
 console.log("User Age: ", userAge);
 console.log("User is Online: ", isOnline);
-
-let wow = "wow";
-let wowMayusc = wow.toUpperCase();
+var wow = "wow";
+var wowMayusc = wow.toUpperCase();
 console.log(wowMayusc);
-
 userName = "Homero";
 console.log("User Name: ", userName);
-
 userAge = "99";
 console.log("User Age:", userAge);
-
 /* Si no queremos que una variable se quiere redefenir usamos CONST */
-const ComoVas = "Ugh"; /* esto es un string, por ejemplo */
-/* now i log it */
-console.log("Cómo vas?: ", ComoVas);
 
+var ComoVas = "Ugh";
+/* esto es un string, por ejemplo */
+
+/* now i log it */
+
+console.log("Cómo vas?: ", ComoVas);
 /* JAVASCRIPT NOS PERMITE CAMBIAR EL TIPO DE DATO AL RENOMBRAR O REVALORAR VARIABLES, PERO ESO ES UNA MUY MALA PRÁCTICA, OSEA SI ES ORIGINALMENTE UNA VARIABLE STRING, NO PASARLA A NUMBER O BOLEANA, QUE SEA CONSTANTE, por ejemplo, debajo: */
 
 console.log(10 + 10);
@@ -35,68 +46,69 @@ console.log(10 + 10);
 
 /* -----------OPERADORES LÓGICOS (de comparación)-------- */
 
-let NUMBER1 = 10;
-
-let NUMBER2 = 20;
-
-let MyBoolean = true;
-
+var NUMBER1 = 10;
+var NUMBER2 = 20;
+var MyBoolean = true;
 console.log(NUMBER1 > NUMBER2);
 console.log(NUMBER1 < NUMBER2);
 console.log(NUMBER2 >= NUMBER1);
 console.log(NUMBER1 <= NUMBER2);
 /* == compara valores, === compara valores y tipos*/
+
 /* SIEMPRE utilizamos el triple igual para comparar valores */
+
 console.log(NUMBER1 == NUMBER2);
 console.log(NUMBER1 === NUMBER2);
-
 console.log(!MyBoolean);
 /* ! -> invierte el valor del operador lógico, si es true, se haría false, es cuestión de ponerlo antes de la variable al loggearla, se utiliza mucho.  */
 //&& el resultado de cualquier operación lógica será true, si y solo si todos sus operandos son true
-console.log(MyBoolean && MyBoolean); /* condición ilogica */
-console.log(
-  false || true
-); /* Ológico: con al menos uno que sea verdadero devuelve true, así el resto sea False, condición ológica */
+
+console.log(MyBoolean && MyBoolean);
+/* condición ilogica */
+
+console.log(false || true);
+/* Ológico: con al menos uno que sea verdadero devuelve true, así el resto sea False, condición ológica */
 
 /* Operacioones aritméticas */
-let NUM1 = 5;
-let NUM2 = 10;
 
-let sumResult = NUM1 + NUM2;
+var NUM1 = 5;
+var NUM2 = 10;
+var sumResult = NUM1 + NUM2;
 console.log("sumResult: ", sumResult);
-
-let subResult = NUM1 - NUM2;
+var subResult = NUM1 - NUM2;
 console.log("subResult: ", subResult);
-
-let multResult = NUM1 * NUM2;
+var multResult = NUM1 * NUM2;
 console.log("multResult: ", multResult);
+var expResult = Math.pow(NUM1, NUM2);
+/* No se usa practicamente */
 
-let expResult = NUM1 ** NUM2; /* No se usa practicamente */
 console.log("expResult: ", expResult);
-
 /* utilizar la propia variable para modificar su valor */
-let numerito1 = 1;
+
+var numerito1 = 1;
 numerito1 = 20;
 /* o puedo hacer esto */
+
 numerito1 = numerito1 + 9999;
 console.log("numerito1: ", numerito1);
 /* o puedo hacer*/
-numerito1++; /* Autoincremento. Sumale uno más al valor de number, eso significa el ++*/
-numero1--; /* Autodecrecimiento */
+
+numerito1++;
+/* Autoincremento. Sumale uno más al valor de number, eso significa el ++*/
+
+numero1--;
+/* Autodecrecimiento */
+
 /* Operador MODULO, sirve por ejemplo para saber si un numero es par o impar, te da el restante de la divisón... */
 
-let modResult = NUM1 % NUM2;
+var modResult = NUM1 % NUM2;
 console.log("modResult: ", modResult);
-
 /* Métodos de los string */
 
-const myName = "Carlos";
-const myLastName = "GR";
-
+var myName = "Carlos";
+var myLastName = "GR";
 console.log(myName + " " + myLastName);
-
 console.log(fullName);
-
 /* Tres maneras de escribir un string en javascript:
 i. ''
 ii. ""
@@ -104,13 +116,14 @@ iii. `` */
 
 /* Caracter especial -> la contrabarra: \  */
 
-const fullName = `Carlos ${myLastName}`;
+var fullName = "Carlos ".concat(myLastName);
 
-let _myCompleteName = `${myLastName} ${myName}`;
+var _myCompleteName = "".concat(myLastName, " ").concat(myName);
+
 console.log(_myCompleteName);
 /* `` Entiendo que estas comillas angulares son mejores, se usa más cuando el string es más completo, le dicen string literal porque lo que esté dentro se loggea tal cuál en conselo */
 
 /* Métodos de los String */
 
-let myString = "Hola Mundo!";
+var myString = "Hola Mundo!";
 console.log(myString.length);
